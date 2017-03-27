c9e1f7492d1482ee3b96e6b7e5c07e12a1ae67a7
ppzhao <ppzhao@caiamodeMacBook-Pro.local>
2017-03-27 18:12:40 +0800

    增加日志库logger https://github.com/orhanobut/logger

--- app/src/main/java/com/ppwalk/healthy/family/MainActivity.java
+++ app/src/main/java/com/ppwalk/healthy/family/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.orhanobut.logger.Logger;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,8 +27,29 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "健康的身体是革命的本钱，好身体才是最重要！", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                if (BuildConfig.DEBUG) {
+                    testLog();
+                }
             }
         });
+    }
+
+    private void testLog() {
+        if (BuildConfig.DEBUG) {
+
+            Logger.v("hello 中国");
+            Logger.d("hello 中国");
+            Logger.i("hello 中国");
+            Logger.w("hello 中国");
+            Logger.e("hello 中国");
+            testSpecialLog();
+            Logger.d(new String[]{"sdfsd", "红果果"});
+        }
+    }
+
+    private static void testSpecialLog() {
+        Logger.wtf("hello wtf");
+        Logger.json("[{\"a\":\"b\",\"b\":\"b\",\"c\":\"b\"},{\"a\":\"b\",\"b\":{\"a\":{\"a\":\"b\",\"b\":\"b\",\"d\":\"b\"},\"x\":\"b\",\"w\":{\"a\":\"b\",\"v\":\"b\",\"ee\":\"b\"}},\"cc\":\"b\"},{\"aa\":\"b\",\"ar\":\"b\",\"ah\":\"b\"}]");
     }
 
     @Override
